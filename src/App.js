e417e8ca281ac69bca3ba8595c0ffb5d03f51e1c
Shukur05 <shukuralyv@gmail.com>
2020-04-01 03:05:33 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
-import React, {Component} from 'react';
-import './App.css';
-import {Route} from 'react-router-dom';
-import {Switch} from 'react-router';
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router";
 import SinglePage from "./components/SinglePage/SinglePage";
 // import NoteWrapper from 'components/NoteWrapper';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            notes: [],
-            actualNotes: [],
-            archiveNotes: [],
-            selectedId: null
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://localhost:3000/notes')
-            .then(response => response.json())
-            .then(result => {
-                this.setState({
-                    notes: result
-                })
-            });
-        this.FilterActual();
-        this.FilterArchive();
-    }
-
-    FilterActual = () =>  {
-        const actual = this.state.notes;
-        this.setState({
-            actualNotes: actual.filter((item)=> item.status !== false)
-        }
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+      actualNotes: [],
+      archiveNotes: [],
+      selectedId: null
     };
+  }
 
-    FilterArchive = () =>  {
-        const actual = this.state.notes;
+  componentDidMount() {
+    fetch("http://localhost:3000/notes")
+      .then(response => response.json())
+      .then(result => {
         this.setState({
-                actualNotes: actual.filter((item)=> item.status !== true)
-            }
-        )
-    };
+          notes: result
+        });
+      });
+    this.FilterActual();
+    this.FilterArchive();
+  }
 
-    render() {
-        return (
-            <>
-                <Switch>
-                    <Route exact path={'/'} notes={this.state.actualNotes}/>
-                    <Route path={'/archive'} notes={this.state.archiveNotes}/>
-                    <Route path={'/create'}/>
-                    <Route path={'/edit'}/>
-                    <Route path={`/notes/${this.state.selectedId}`} component={SinglePage}/>
-                </Switch>
-            </>
-        );
-    }
+  FilterActual = () => {
+    const actual = this.state.notes;
+    this.setState({
+      actualNotes: actual.filter(item => item.status !== false)
+    });
+  };
+
+  FilterArchive = () => {
+    const actual = this.state.notes;
+    this.setState({
+      actualNotes: actual.filter(item => item.status !== true)
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path={"/"} notes={this.state.actualNotes} />
+          <Route path={"/archive"} notes={this.state.archiveNotes} />
+          <Route path={"/create"} />
+          <Route path={"/edit"} />
+          <Route
+            path={`/notes/${this.state.selectedId}`}
+            component={SinglePage}
+          />
+        </Switch>
+        test
+      </>
+    );
+  }
 }
 
 export default App;
