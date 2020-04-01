634218f266ec9186b3d7634173792924b638f87c
Shukur05 <shukuralyv@gmail.com>
2020-04-01 12:05:21 +0400

    NoteWrapper improved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./Header.scss";
 
 const Header = () => {
   return (
     <header className={"header"}>
-      <h1>NotesApp</h1>
-      <button className="actual">Actual</button>
-      <button className="archive">Archive</button>
-      <button className="create">Create</button>
+      <div className="header-left">
+        <h1>NotesApp</h1>
+        <button className="actual">Actual</button>
+        <button className="archive">Archive</button>
+      </div>
+      <div className="header-right">
+        <button className="create">Create</button>
+      </div>
     </header>
   );
 };
