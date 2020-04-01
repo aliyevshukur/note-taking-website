64472766d8f7665295132950e5a3e6f9e65282cd
Shukur05 <shukuralyv@gmail.com>
2020-04-01 18:22:38 +0400

    update colors to rgb values

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Note.scss";
 
 const Note = props => {
+  const noteStyle = {
+    backgroundColor: `${props.note.color}`,
+    border: `1px solid ${props.note.color}`
+  };
+
+  const noteTitleStyle = {
+    borderBottom: `1px solid ${props.note.color}`
+  };
+
   return (
-    <div className={"note"}>
-      <h1 className={"note-title"}>{props.note.title}</h1>
+    <div style={noteStyle} className={"note"}>
+      <h1 style={noteTitleStyle} className={"note-title"}>
+        {props.note.title}
+      </h1>
       <p className="context">{props.note.context}</p>
     </div>
   );
